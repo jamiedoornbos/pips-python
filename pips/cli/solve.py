5c4773b02fe75553a1c4172b50e6e048b96b9435
Jamie Doornbos <35746538+jamiedoornbos@users.noreply.github.com>
2026-02-17 08:19:43 -0800

    Update cli

--- pips/cli/solve.py
+++ pips/cli/solve.py
@@ -1,12 +1,20 @@
 import click
 
 from pips.data.boardfromstr import read_board_from_string
+from pips.model import Board
+from pips.solve.node import Node
 from pips.solve.solver import Solver
 
 
 def summary(solver: Solver) -> str:
-    opened = ', '.join(f'{depth}: {(count,)}' for depth, count in enumerate(solver.open_count_by_depth()) if count > 0)
+    opened = '; '.join(f'{depth}: {count:,}' for depth, count in enumerate(solver.open_count_by_depth()) if count > 0)
     return f'Tree ({opened}) | Solutions ({len(solver.solutions)})'
+
+
+def print_placements(title: str, board: Board):
+    print(title)
+    for placement in board.placements:
+        print(f'  {placement}')
 
 
 @click.command()
@@ -17,13 +25,25 @@ def main(filename: str):
 
     print(f'Loaded board: {board}')
     solver = Solver(board)
-    print('Expanding')
+    print('Expanding to find solutions')
+    last_message = ''
     while True:
         message = summary(solver)
-        print(message)
+        print(f'\r{message}', end='')
+        if len(last_message) > len(message):
+            print(' ' * (len(last_message) - len(message)), end='')
+        last_message = message
+
         node = solver.expand_next()
         if node is None:
+            print()
             break
+
+    if solver.solutions:
+        for index, solution in enumerate(solver.solutions, 1):
+            print_placements(f'Solution #{index}', solution.board)
+    else:
+        print('No solutions found!')
 
 
 if __name__ == '__main__':
